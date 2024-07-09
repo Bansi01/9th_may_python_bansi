dae5863e1dbe9ebc42d0cdb655974acc4dba61f1
Bansi01 <solankibansi619@gmail.com>
2024-07-09 12:37:40 +0530

    Implemented note generation feature

--- Assessment/note_generator.py
+++ Assessment/note_generator.py
@@ -1,8 +1,9 @@
-# note_generator.py
+import datetime
+
 def generate_note(title, content):
     note = {
         "title": title,
         "content": content,
-        "created_at": datetime.now()
+        "created_at": datetime.datetime.now()
     }
     return note
